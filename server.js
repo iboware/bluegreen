8992e4b4fd9d5f571fd21e600bc2c2b49de9c0d5
İbrahim <ibrahim.varol@primeapps.io>
2020-11-02 11:43:59 +0100

    v2.0

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ http.createServer(function(req, res){
   var hostname = process.env.HOSTNAME;
   res.writeHead(200, { 'Content-Type': 'text/HTML' });
   var name = process.env.NAME || 'World';
-  var body = '<div style="background-color:blue; color:white"> \
+  var body = '<div style="background-color:green; color:white"> \
     <h1>Hello '+name+'</h1> \
-     <p>This is a demo page v1.0</p> \
+     <p>This is a demo page v2.0</p> \
      <p>from server: '+hostname+'</p></div>';
   res.end(body);
 }).listen(port, function(){
