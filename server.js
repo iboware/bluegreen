82612da6de83ae61f7f57589325483adb82fb1c0
İbrahim <ibrahim.varol@primeapps.io>
2020-11-02 06:53:29 +0100

    App updated

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 var http = require('http');
-var port = 80;
+var port = 8080;
 
 http.createServer(function(req, res){
   var hostname = process.env.HOSTNAME;
   res.writeHead(200, { 'Content-Type': 'text/HTML' });
   var name = process.env.NAME || 'World';
-  var body = 
-    '<h1>Hello '+name+'</h1> \
-     <p>This is a demo page/p> \
-     <p>from server: '+hostname+'</p>';
+  var body = '<div style="background-color:blue; color:white"> \
+    <h1>Hello '+name+'</h1> \
+     <p>This is a demo page</p> \
+     <p>from server: '+hostname+'</p></div>';
   res.end(body);
 }).listen(port, function(){
   console.log('Server running at port ' + port);
